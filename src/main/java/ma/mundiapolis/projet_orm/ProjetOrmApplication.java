d5cc6328ea0227595e0ca9404ec7969f71c4440f
KARIMACHOUGRI <idrisswater0@gmail.com>
2025-10-22 10:15:33 +0100

    ORM COMMIT 1

---
+++ src/main/java/ma/mundiapolis/projet_orm/ProjetOrmApplication.java
@@ -0,0 +1,13 @@
+package ma.mundiapolis.projet_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetOrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/mundiapolis/projet_orm/ProjetOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.mundiapolis.projet_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
