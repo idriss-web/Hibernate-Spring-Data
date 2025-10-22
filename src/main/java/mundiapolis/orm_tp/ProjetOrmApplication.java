61b3cb8ebca752b1bb1dcd665470cc55ffc4d7ff
IDRISS <idrisswater0@gmail.com>
2025-10-22 11:55:53 +0100

    ORM COMMIT NOUVEAU

--- src/main/java/mundiapolis/orm_tp/ProjetOrmApplication.java
+++ src/main/java/mundiapolis/orm_tp/ProjetOrmApplication.java
@@ -1,6 +1,5 @@
 package mundiapolis.orm_tp;
 
-
 import mundiapolis.orm_tp.entities.Product;
 import mundiapolis.orm_tp.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,40 +11,47 @@ import java.util.List;
 
 @SpringBootApplication
 public class ProjetOrmApplication implements CommandLineRunner {
-@Autowired
-private ProductRepository productRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(ProjetOrmApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-       productRepository.save(new Product(null,"Computer",4300,3));
-       productRepository.save(new Product(null,"Printer",1200,4));
-       productRepository.save(new Product(null,"Smart Phone",3200,32));
-       List<Product> products = productRepository.findAll();
-       products.forEach(
+        productRepository.save(new Product(null, "Computer", 4300, 3));
+        productRepository.save(new Product(null, "Printer", 1200, 4));
+        productRepository.save(new Product(null, "Smart Phone", 3200, 32));
 
-               p->
-               {
-                   System.out.println(p.toString());
-               }
+        List<Product> products = productRepository.findAll();
+        products.forEach(p -> System.out.println(p));
 
-       );
-
-        Product product = productRepository.findById(Long.valueOf(1)).get();
+        Product product = productRepository.findById(1L).get();
         System.out.println("***************");
         System.out.println(product.getId());
         System.out.println(product.getName());
         System.out.println(product.getQuantity());
         System.out.println("***************");
-List<Product> productList = productRepository.findByNameContains("C");
-productList.forEach(
-        p->
-        {
-            System.out.println(p);
-        }
-);
+
+        List<Product> productList = productRepository.findByNameContains("C");
+        productList.forEach(p -> System.out.println(p));
+
+        System.out.println("----------------------");
+
+        List<Product> productList2 = productRepository.search("%C%");
+        productList2.forEach(p -> System.out.println(p));
+
+
+
+
+
+        System.out.println("----------------------");
+
+        List<Product> productList3 = productRepository.findByPriceGreaterThan(3000);
+        productList3.forEach(p -> System.out.println(p));
+
 
 
     }
--- src/main/java/mundiapolis/orm_tp/repository/ProductRepository.java
+++ src/main/java/mundiapolis/orm_tp/repository/ProductRepository.java
@@ -2,10 +2,17 @@ package mundiapolis.orm_tp.repository;
 
 import mundiapolis.orm_tp.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductRepository  extends JpaRepository<Product,Long> {
     List<Product> findByNameContains(String mc);
+    List<Product> findByPriceGreaterThan(double price);
+
+
+    @Query("select p from Product p where p.name like :x")
+    List<Product> search(@Param("x") String mc);
 
 }
