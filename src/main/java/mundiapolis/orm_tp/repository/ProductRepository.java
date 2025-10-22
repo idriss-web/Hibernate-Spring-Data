290359c52574141cb5d676e6d3f16938599aaa46
IDRISS <idrisswater0@gmail.com>
2025-10-22 12:00:12 +0100

    ORM COMMIT NOUVEAU2

--- src/main/java/mundiapolis/orm_tp/repository/ProductRepository.java
+++ src/main/java/mundiapolis/orm_tp/repository/ProductRepository.java
@@ -15,4 +15,9 @@ public interface ProductRepository  extends JpaRepository<Product,Long> {
     @Query("select p from Product p where p.name like :x")
     List<Product> search(@Param("x") String mc);
 
+
+    @Query("select p from Product p where p.price>:x")
+    List<Product> searchByPrice(@Param("x") double price);
+
+
 }
